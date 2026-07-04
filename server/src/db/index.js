2d9f34ae64e9f06c67d32103528d723facba9587
Anmol Singh <anmol2006.chauhan@gmail.com>
2026-07-04 20:02:14 +0530

    bug fixing

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/smarterp");
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE_URL || "mongodb://localhost:27017/smarterp");
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
